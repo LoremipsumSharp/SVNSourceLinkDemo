fe261383125b08e3d4f788f66e62231247e32a9a
loremipsum <289989941@qq.com>
2021-05-16 20:58:19 +0800

    init commit

---
+++ src/SVNSourceLinkDemo.Common/Utils.cs
@@ -0,0 +1,10 @@
+namespace SVNSourceLinkDemo.Common
+{
+    public class Utils
+    {
+        public static string Greet(string name)
+        {
+            return $"Hello {name}";
+        }
+    }
+}
